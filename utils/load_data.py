0335678219776a4e96c41e7ed4e1e7ad1a5d5757
JohAn <johandinox@gmail.com>
2021-07-08 09:24:06 +0200

    Fixed reference to class member

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,5 +1,7 @@
-class Steinmetz():
+import numpy as np
 
+
+class Steinmetz():
     def __init__(self, alldat):
         self.alldat = alldat
         return
@@ -10,13 +12,13 @@ class Steinmetz():
         returns: dict keyed by session ids containing recordings from all query region and valued by number of neurons per query region
         """
         hits = {}
-        for sess in range(len(alldat)):
+        for sess in range(len(self.alldat)):
             num_neurons = {}
-            intersections = np.full(shape=alldat[sess]['brain_area'].shape, fill_value=True)
+            intersections = np.full(shape=self.alldat[sess]['brain_area'].shape, fill_value=True)
             for query_region in query_regions:
                 intersections = np.intersect1d(intersections,
-                                               alldat[sess]['brain_area'] == query_region)
-                num_neurons[query_region] = (alldat[sess]['brain_area'] == query_region).sum()
+                                               self.alldat[sess]['brain_area'] == query_region)
+                num_neurons[query_region] = (self.alldat[sess]['brain_area'] == query_region).sum()
             if intersections.sum() > 0:
                 hits[sess] = num_neurons
         return hits
