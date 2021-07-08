87eaedf06b3b4ba414f673cbcd253875df66041d
JohAn <johandinox@gmail.com>
2021-07-08 09:21:57 +0200

    Fixed format error

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,22 +1,22 @@
 class Steinmetz():
 
-	def __init__(self,alldat):
-		self.alldat = alldat
-		return
+    def __init__(self, alldat):
+        self.alldat = alldat
+        return
 
-  	def find_session(self,query_regions):
-		"""
-		param: query_regions is a list of region names strings 
-		returns: dict keyed by session ids containing recordings from all query region and valued by number of neurons per query region
-		"""
-		hits = {}
-		for sess in range(len(alldat)):
-			num_neurons = {}
-			intersections = np.full(shape=alldat[sess]['brain_area'].shape,fill_value=True)
-			for query_region in query_regions:
-				intersections = np.intersect1d(intersections, alldat[sess]['brain_area']==query_region)
-				num_neurons[query_region] = (alldat[sess]['brain_area']==query_region).sum()
-			if intersections.sum()>0:
-		  		hits[sess] = num_neurons
-		return hits
-
+    def find_session(self, query_regions):
+        """
+        param: query_regions is a list of region names strings
+        returns: dict keyed by session ids containing recordings from all query region and valued by number of neurons per query region
+        """
+        hits = {}
+        for sess in range(len(alldat)):
+            num_neurons = {}
+            intersections = np.full(shape=alldat[sess]['brain_area'].shape, fill_value=True)
+            for query_region in query_regions:
+                intersections = np.intersect1d(intersections,
+                                               alldat[sess]['brain_area'] == query_region)
+                num_neurons[query_region] = (alldat[sess]['brain_area'] == query_region).sum()
+            if intersections.sum() > 0:
+                hits[sess] = num_neurons
+        return hits
